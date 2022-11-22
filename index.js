2be0291cfa0f4497b4d596bb3c4563f0178ba869
ZhiLeong <saber_zhileong@hotmail.com>
2022-11-21 21:00:10 -0500

    commit tree b4deacfb4f2d4a6c4ee1c25846154f8c0be80d84

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random = 1;
+random++;
+
+var num3 = 16;
+var num4 = 4;
+var mod = num3 / num4;
+
+var max = Math.max(1,20);
